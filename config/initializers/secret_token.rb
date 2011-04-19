0dbd4e7f1f592277c2d42f93d52ca25475fbf3e9
Git Fly <gitfly@gmail.com>
2011-04-18 23:22:34 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '06939b883d10f5c3ac3f797cbe1d1596333fc0c506f77dd6e76c3cc505423a3cc733fa0341e3a60310c35367da6a5af7d34895a93816659917d92c4ee9c9118b'
